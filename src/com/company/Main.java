cfc3491a46d0c637b3e0cdbbbc5817ccbaafa95d
klaudia.budzynska <klaudia.budzynska1@gmail.com>
2022-03-13 17:24:19 +0100

    calculate crc32

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,30 @@
 package com.company;
 
+class CRC{
+    int[] CRC_TABLE;
 
+    public CRC() {
+        CRC_TABLE = new int[256];
+        for (int i = 0; i < 256; ++i) {
+            int code = i;
+            for (int j = 0; j < 8; ++j) {
+                code = ((code & 0x01) != 0 ? 0xEDB88320 ^ (code >>> 1) : (code >>> 1));
+            }
+            CRC_TABLE[i] = code;
+        }
+    }
+
+    public int calculate(String text) {
+        int crc = -1;
+        for (int i = 0; i < text.length(); ++i) {
+      	    int code = Character.codePointAt(text, i);
+            crc = CRC_TABLE[(code ^ crc) & 0xFF] ^ (crc >>> 8);
+            System.out.println(crc);
+        }
+        System.out.println(Integer.toBinaryString((-1 ^ crc) >>> 0));
+        return (-1 ^ crc) >>> 0;
+    }
+}
 
 public class Main {
 
@@ -9,6 +33,9 @@ public class Main {
         int index = searchIndex(array, 11); // we want to find index for 11
 
         System.out.println(index);
+
+        CRC crc = new CRC();
+        System.out.println(crc.calculate("This is example text ..."));
     }
 
     private static int searchIndex(int[] array, int value) {
