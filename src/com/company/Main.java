60607bdeae6c6ad92e81989847042bdade32b0c3
klaudia.budzynska <klaudia.budzynska1@gmail.com>
2022-03-13 16:52:33 +0100

    Binary Search

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {4, 5, 7, 11, 12, 15, 15, 21, 40, 45 };
+        int index = searchIndex(array, 11); // we want to find index for 11
+
+        System.out.println(index);
+    }
+
+    private static int searchIndex(int[] array, int value) {
+        int index = 0;
+        int limit = array.length - 1;
+        while (index <= limit) {
+            int point = (int)Math.ceil((index + limit) / 2);
+            int entry = array[point];
+            if (value > entry) {
+                index = point + 1;
+                continue;
+            }
+            if (value < entry) {
+                limit = point - 1;
+                continue;
+            }
+            return point; // value == entry
+        }
+        return -1;
+    }
+}
